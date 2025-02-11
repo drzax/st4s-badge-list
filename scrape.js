9a904ae656377816358ce4bdd8a4f17c37782664
Simon Elvery <simon@elvery.net>
2025-02-11 17:02:59 +1000

    Scrape ST4S badge data

--- scrape.js
+++ scrape.js
@@ -9,10 +9,10 @@ const page = await context.newPage();
 await context.route('**.jpg', route => route.abort());
 
 // TODO: Change this to the URL you want to scrape
-await page.goto('https://example.com/');
+await page.goto('https://st4s.edu.au/verify-a-badge/');
 
 // DO YOUR SCRAPING HERE AND WRITE YOUR DATA TO A FILE
-writeFileSync('./data/example.html', await page.mainFrame().content())
+writeFileSync('./data/badges.json', JSON.stringify(JSON.parse(await page.locator('#badge-data').innerText()), null, 2));
 
 // Teardown
 await context.close();
